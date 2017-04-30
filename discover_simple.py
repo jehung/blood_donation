d0b7247cedfe3cbf40102a647b8f3d488e332e67
unknown <jehung@me.com>
2017-04-29 20:17:17 -0700

    Modify LogisticRegressionCV

--- discover_simple.py
+++ discover_simple.py
@@ -14,14 +14,25 @@ from sklearn import cross_validation
 from sklearn import model_selection
 from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, log_loss
 
 file_in = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\train.csv'
 
-numerical_features = ['Number of Donations', 'Months since First Donation', 'Months since Last Donation']
+numerical_features = ['Number of Donations', 'Months since First Donation', 'Months since Last Donation', 'If']
 
 data = pd.read_csv(file_in)
 y = data['Made Donation in March 2007']
 X = data.loc[:,numerical_features]
+
+
+
+
+def make_feature(data):
+    temp = (data['Months since First Donation']-data['Months since Last Donation'])/data['Number of Donations']
+    temp -= data['Months since Last Donation']
+    data['if'] = abs(temp)
+    return data
+
 
 
 def error_function(valset, prob):
@@ -32,19 +43,25 @@ def error_function(valset, prob):
 
 
 def model():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    numerical_features = ['Number of Donations', 'Months since First Donation', 'Months since Last Donation', 'if']
+    from sklearn.preprocessing import PolynomialFeatures
+    X_x = PolynomialFeatures(2).fit_transform(X)
+    print('final check', X_x.shape)
+    X_train, X_test, y_train, y_test = train_test_split(X_x, y, test_size=0.2)
     X_train1, X_val, y_train1, y_val = train_test_split(X_train, y_train, test_size=0.2)
     logit = LogisticRegression(solver='liblinear')
-    cv = model_selection.KFold(n_splits=20, shuffle=True)
-    #sss = StratifiedShuffleSplit(n_splits=10, test_size=0.1)
+    #cv = model_selection.KFold(n_splits=200, shuffle=True)
+    sss = StratifiedShuffleSplit(n_splits=200, test_size=0.2)
 
-    param_grid = {  # "ada__base_estimator__criterion" : ["gini", "entropy"],
+    param_grid = {
         'C': [0.000000001, 0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 1, 10, 100, 1000, 10000,
               100000],
         'penalty': ['l1', 'l2'],
         'max_iter': [100, 1000, 10000, 100000],
         'fit_intercept': [True, False],
     }
+
+    score_func = make_scorer(log_loss, greater_is_better=False, needs_proba=True)
 
     #clf = GridSearchCV(logit,
     #                   scoring='accuracy',
@@ -54,8 +71,8 @@ def model():
     searchCV = LogisticRegressionCV(
         Cs=list(np.power(10.0, np.arange(-10, 10)))
         ,penalty='l1'
-        ,scoring='neg_log_loss'
-        ,cv=cv
+        ,scoring=score_func
+        ,cv=sss
         ,random_state=787
         ,max_iter=10000
         ,fit_intercept=True
@@ -63,15 +80,16 @@ def model():
         ,tol=1e-4
     )
 
+
     searchCV.fit(X_train,y_train)
-    print(searchCV.scores_[1].max())
+    print('1', searchCV.scores_[1].max())
 
 
     results = []
 
     proba = searchCV.predict_proba(X_test)[:,1]
-    loss = error_function(y_test, proba)
-    print(loss)
+    loss = log_loss(y_test, proba)
+    print('2', loss)
     #print(searchCV.predict(X_test))
     #print(y_test.tolist())
 
@@ -89,5 +107,6 @@ def model():
 
 
 '''
+make_feature(data)
 clf = model()
 
