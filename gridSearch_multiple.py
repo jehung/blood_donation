2e6118c6d5979813d39359424127c549f8433c39
unknown <jehung@me.com>
2017-05-07 23:37:25 -0700

    Modify gridsearch function

--- gridSearch_multiple.py
+++ gridSearch_multiple.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
@@ -8,7 +7,7 @@ from sklearn.cluster import KMeans
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, VotingClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
@@ -21,10 +20,14 @@ from sklearn.metrics import make_scorer, log_loss
 from sklearn.metrics import classification_report
 from joblib import Parallel, delayed
 import multiple_models
+from sklearn.preprocessing import PolynomialFeatures, scale
 
-file_in = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\train.csv'
+
+
+file_in = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\train_cleaned.csv'
 data = pd.read_csv(file_in)
-score_func = make_scorer(log_loss, greater_is_better=True, needs_proba=True)
+score_func = make_scorer(log_loss, greater_is_better=False, needs_proba=True)
+
 
 def make_feature(data):
     temp = (data['Months since First Donation']-data['Months since Last Donation'])/data['Number of Donations']
@@ -32,60 +35,121 @@ def make_feature(data):
     data['if'] = abs(temp)
 
     temp1 = data['If']
-    data['Exp_if'] = temp1-temp1.mean()/(temp1.max()-temp1.min())
+    data['log'] = (temp1-temp1.mean())/temp1.std()
+    print(data.head())
+
+    temp2 = (data['If'] + (data['Number of Donations'])**2 - 10.2)/data['Months since First Donation']
+    data['eureka'] = 1 / (1 + np.exp(-1*temp2))
+
+    data['feature'] = data['eureka'] - data['log']
+    #print(data.head())
     return data
-
-
-def error_function(valset, prob):
-    temp = (valset.values*np.log(prob) + (1 - valset.values)*np.log(1 - prob)).sum()
-    return -1/len(prob)*temp
 
 
 def gridSearch_models(data):
     numerical_features = ['Number of Donations', 'Months since First Donation', 'Months since Last Donation',
-                          'if', 'Exp_if']
+                          'If', 'log']
+
     y = data['Made Donation in March 2007']
     X = data.loc[:, numerical_features]
-    from sklearn.preprocessing import PolynomialFeatures
-    X_x = PolynomialFeatures(2).fit_transform(X)
-    print('X_x', X_x.shape)
-    X_train, X_test, y_train, y_test = train_test_split(X_x, y, test_size=0.2)
-    print('X_train', X_train)
+    print('X', X.head())
+    #X_x = PolynomialFeatures(2).fit_transform(X)
+    from sklearn import preprocessing
+    X_x = preprocessing.normalize(X)
+    X_x1 = PolynomialFeatures(2).fit_transform(X_x)
+    #X_train, X_test, y_train, y_test = train_test_split(X_x, y, test_size=0.2)
+    #print('X_train', X_train.shape)
 
     models1 = {
-        'logit': LogisticRegression(),
+        'logit': LogisticRegression(warm_start=True),
         'svc': SVC(probability=True),
-        'gb':  GradientBoostingClassifier(),
+        'mlp':  MLPClassifier(activation='logistic'),
         'forest': RandomForestClassifier(),
+        'dt': DecisionTreeClassifier(max_features="auto")
     }
 
     params1 = {
-        'logit': {'C': [0.000000001, 0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001,
-                        0.001, 0.01, 1, 10, 100, 1000, 10000, 100000],
+        'logit': {'C': list(np.power(3.0, np.arange(-7, 7))),
                   'penalty': ['l1', 'l2'],
                   'class_weight': [None, 'balanced'],
                   'fit_intercept': [True, False]},
         'svc': {'kernel': ['linear', 'rbf', 'sigmoid'],
-                'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-                'gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],},
-        'gb': {'n_estimators': [10, 20, 50, 70, 100, 120, 150],
-               'max_depth': [3, 4, 5, 6, 7, 8, 9, 10, 15],
-               'learning_rate': [0.1, 0.3, 0.6, 0.9, 1.2, 1.5],
-               'max_depth': [3, 4, 5, 6, 7]},
+                'C': list(np.power(3.0, np.arange(-15, -5))),
+                'gamma': list(np.power(3.0, np.arange(-15, -5))),
+                'class_weight': [None, 'balanced']},
+        'mlp': {'solver': ['sgd', 'adam'],
+               'learning_rate': ['invscaling', 'adaptive'],
+               'learning_rate_init': [0.001, 0.003, 0.009, 0.03, 0.09, 0.3, 0.9, 1.5]},
         'forest': {'n_estimators': [10, 20, 50, 70, 100, 120, 150],
                    'max_features': ['auto', 'log2', 0.1, 0.3, 0.5, 0.7, 0.9, None],
-                   'criterion': ['gini', 'entropy']}
+                   'criterion': ['gini', 'entropy']},
+        'dt': {'min_samples_split': [5, 10, 15, 20, 25, 35, 50],
+              'max_depth': [3, 4, 5, 6, 7],
+              'class_weight': [None, 'balanced']}
     }
 
 
-    #cv = model_selection.KFold(n_splits=20, shuffle=False, random_state=42)
-    sss = StratifiedShuffleSplit(n_splits=50, test_size=0.2)
+    cv = model_selection.KFold(n_splits=20, shuffle=False, random_state=42)
+    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
 
-    if __name__ == '__main__':
-        helper1 = multiple_models.EstimatorSelectionHelper(models1, params1)
-        helper1.fit(X_x, y, scoring=score_func, n_jobs=-1)
+    helper1 = multiple_models.EstimatorSelectionHelper(models1, params1)
+    models = helper1.fit(X_x1, y, scoring='neg_log_loss', n_jobs=-1, cv=sss)
+    
+    a = helper1.score_summary(sort_by='median_score')
+    print(a)
+    #b = helper1.get_best(models, X_x, y)
+    df = pd.DataFrame(columns=('w1', 'w2', 'w3', 'w4', 'w5', 'mean', 'std'))
 
-    print('Processes done')
+    i = 0
+    for w1 in range(1,6):
+        for w2 in range(1,6):
+            for w3 in range(1,6):
+                for w4 in range(1,6):
+                    for w5 in range(1,6):
 
-make_feature(data)
-clf = gridSearch_models(data)
+                        if len(set((w1,w2,w3,w4,w5))) == 1: # skip if all weights are equal
+                            continue
+
+                        eclf = VotingClassifier(estimators=models, weights=[w1,w2,w3,w4,w5], voting='soft')
+                        scores = cross_validation.cross_val_score(
+                                                estimator=eclf,
+                                                X=X_x1,
+                                                y=y,
+                                                cv=5,
+                                                scoring='neg_log_loss',
+                                                n_jobs=1)
+
+                        df.loc[i] = [w1, w2, w3, w4, w5, scores.mean(), scores.std()]
+                        i += 1
+
+    df.sort(columns=['mean', 'std'], ascending=False)
+    
+    writer = pd.ExcelWriter('output_weights.xlsx')
+    df.to_excel(writer, 'Sheet1')
+
+    eclf = VotingClassifier(estimators=models, voting='soft')
+    eclf.fit(X_x1, y)
+
+    test_file = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\test.csv'
+
+    test = pd.read_csv(test_file)
+    test1 = make_feature(test)
+    test_X = test1.loc[:, numerical_features]
+    test_X_x = preprocessing.normalize(test_X)
+    test_X_x1 = PolynomialFeatures(2).fit_transform(test_X)
+
+
+    predict_proba = eclf.predict_proba(test_X_x1)
+    print(predict_proba)
+    test_col = pd.DataFrame(predict_proba)
+    df_id = test.loc[:, ['ID']]
+    test_mid = pd.concat([df_id, test_col], axis=1)
+    test_mid.head()
+    submission = test_mid.loc[:, ['ID', 1]]
+    submission.rename(columns={'ID': '', 1: 'Made Donation in March 2007'}, inplace=True)
+    submission.to_csv('submission.csv')
+
+if __name__ == '__main__':
+    make_feature(data)
+    gridSearch_models(data)
+
--- multiple_models.py
+++ multiple_models.py
@@ -1,5 +1,13 @@
 import pandas as pd
-from sklearn.grid_search import GridSearchCV
+import numpy as np
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedShuffleSplit
+import collections
+from sklearn.metrics import make_scorer, log_loss
+
+
+
+score_func = make_scorer(log_loss, greater_is_better=False, needs_proba=True)
+final = []
 
 class EstimatorSelectionHelper:
     def __init__(self, models, params):
@@ -10,34 +18,70 @@ class EstimatorSelectionHelper:
         self.params = params
         self.keys = models.keys()
         self.grid_searches = {}
+        self.final_models = collections.defaultdict(dict)
 
-    def fit(self, X, y, cv=3, n_jobs=1, verbose=1, scoring=None, refit=False):
+
+
+    def fit(self, X, y, cv=None, n_jobs=1, verbose=1, scoring=None, refit=True):
+        best_score = -20
+        best_choice = None
         for key in self.keys:
             print("Running GridSearchCV for %s." % key)
             model = self.models[key]
             params = self.params[key]
-            gs = GridSearchCV(model, params, cv=cv, n_jobs=n_jobs,
+
+            gs = GridSearchCV(model, params, cv=20, n_jobs=n_jobs,
                               verbose=verbose, scoring=scoring, refit=refit)
             gs.fit(X,y)
             self.grid_searches[key] = gs
+            final.append((key, gs.best_estimator_))
+            print(gs.best_estimator_)
+            
+        return final
+
+
 
     def score_summary(self, sort_by='mean_score'):
         def row(key, scores, params):
             d = {
                  'estimator': key,
-                 'min_score': min(scores),
-                 'max_score': max(scores),
-                 'mean_score': mean(scores),
-                 'std_score': std(scores),
+                 'min_score': scores.min(),
+                 'max_score': scores.max(),
+                 'mean_score': scores.mean(),
+                 'std_score': scores.std(),
+                 'median_score': np.median(scores)
             }
-            return pd.Series(dict(params.items() + d.items()))
+
+            return pd.Series({**params, **d})
 
         rows = [row(k, gsc.cv_validation_scores, gsc.parameters)
                      for k in self.keys
                      for gsc in self.grid_searches[k].grid_scores_]
-        df = pd.concat(rows, axis=1).T.sort([sort_by], ascending=False)
+        df = pd.concat(rows, axis=1).T.sort_values(by=[sort_by], ascending=False)
 
-        columns = ['estimator', 'min_score', 'mean_score', 'max_score', 'std_score']
+        columns = ['estimator', 'min_score', 'mean_score', 'median_score', 'max_score', 'std_score']
         columns = columns + [c for c in df.columns if c not in columns]
 
+        writer = pd.ExcelWriter('output.xlsx')
+        df[columns].to_excel(writer, 'Sheet1')
+
         return df[columns]
+
+    def get_best(self, final, X, y):
+        results = []
+        sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2)
+        for f in final:
+            for traincv, testcv in sss.split(X, y):
+                proba = f.fit(X[traincv,:],y.iloc[traincv]).predict_proba(X[testcv,:])[:,1]
+                pred = f.fit(X[traincv,:],y.iloc[traincv]).predict(X[testcv,:])
+                #print(np.log(proba))
+                #results.append(error_function(y_train.iloc[testcv], proba))
+                results.append(log_loss(y.iloc[testcv], pred))
+                print(f)
+                print(log_loss(y.iloc[testcv], pred))
+                print('min', min(results))
+                print('max', max(results))
+        print("Results: ", results)
+
+        #writer = pd.ExcelWriter('finaloutput.xlsx')
+        #final[columns].to_excel(writer, 'Sheet1')
