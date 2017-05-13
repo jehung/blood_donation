6c2fa6e99eadab02be9ef3851b28d118804a3e98
unknown <jehung@me.com>
2017-05-13 12:48:17 -0700

    Update logit and bagging classifier

--- discover.py
+++ discover.py
@@ -1,145 +1,107 @@
-import csv
-from read_data import read_data
-from feature_format import featureFormat, targetFeatureSplit
-from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, train_test_split
 from sklearn import decomposition
 from sklearn.cluster import KMeans
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, VotingClassifier, BaggingClassifier
 from sklearn.neural_network import MLPClassifier
-import numpy as np
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, train_test_split
-from sklearn.preprocessing import MinMaxScaler, Normalizer, StandardScaler
-import sklearn.preprocessing as preprocessing
-import pickle as pickle
-import imblearn
-from imblearn.pipeline import make_pipeline
-from imblearn.over_sampling import SMOTE
-from sklearn.ensemble import GradientBoostingClassifier
-import xgboost as xgb
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import ExtraTreesClassifier
-from mlxtend.classifier import EnsembleVoteClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import cross_validation
+from sklearn import model_selection
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, log_loss
+from sklearn.metrics import classification_report
+from joblib import Parallel, delayed
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, scale
+from mlxtend.feature_selection import SequentialFeatureSelector
 
-file_in = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\train.csv'
-
-numerical_features = ['Made Donation in March 2007', 'Months since Last Donation', 'Number of Donations']
-data_raw = read_data(file_in, numerical_features)
-data = featureFormat(data_raw, features=numerical_features)
-
-labels, features = targetFeatureSplit(data)
-
-features_x = preprocessing.PolynomialFeatures().fit_transform(features)
-
-
-class BaseEstimator_compability(LogisticRegression):
-    def predict(self, X):
-        return self.predict_proba(X)[:, 1][:, np.newaxis]
+file_in = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\train_cleaned.csv'
+data = pd.read_csv(file_in)
 
 
-def model():
-    param_grid = {#"ada__base_estimator__criterion" : ["gini", "entropy"],
-        #"ada__base_estimator__splitter" :   ["best", "random"],
-        # "ada__n_estimators": [20, 50, 75, 100],
-        #'pca__n_components': [1, 2],
-        'abC2__base_estimator__max_delta_step': [0.5, 1, 1.5, 2],
-        'abC2__base_estimator__max_depth': [2, 3, 4, 5, 6, 7, 8],
-        'abC2__base_estimator__min_child_weight': [0.5, 1, 1.5],
-        'abC2__base_estimator__gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-        'abC2__base_estimator__subsample': [0.4, 0.5, 0.6],
+def make_feature(data):
+    temp = (data['Months since First Donation'] - data['Months since Last Donation']) / data['Number of Donations']
+    temp -= data['Months since Last Donation']
+    data['if'] = abs(temp)
 
-        # 'eclf__decisiontreeclassifier__min_samples_split': [5, 10, 15, 20, 25, 35, 50],
-        # 'eclf__decisiontreeclassifier__class_weight': [None, 'balanced'],
-        # 'eclf__decisiontreeclassifier__max_depth': [3, 4, 5, 6, 7],
-        # 'eclf__logisticregression__C': [0.000000001, 0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 1, 10, 100, 1000, 10000, 100000],
-        # 'eclf__logisticregression__penalty': ['l1', 'l2'],
-        # 'eclf__logisticregression__class_weight': [None, 'balanced'],
-        #'eclf__adaboostclassifier-1__base_estimator__min_samples_split': [5, 10, 15, 20, 25, 35, 50],
-        #'eclf__adaboostclassifier-1__base_estimator__max_depth': [3, 4, 5, 6, 7],
-        #'eclf__adaboostclassifier-1__base_estimator__class_weight': [None, 'balanced'],
-        #'eclf__adaboostclassifier-2__base_estimator__max_delta_step': [0.5, 1, 1.5, 2],
-        #'eclf__adaboostclassifier-2__base_estimator__max_depth': [2, 3, 4, 5, 6, 7, 8],
-        #'eclf__adaboostclassifier-2__base_estimator__min_child_weight': [0.5, 1, 1.5],
-        #'eclf__adaboostclassifier-2__base_estimator__gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-        #'eclf__adaboostclassifier-2__base_estimator__subsample': [0.4, 0.5, 0.6],
+    temp1 = data['If']
+    return data
 
-        # 'svc__kernel': ('linear', 'rbf', 'sigmoid'),
-        # 'svc__C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-        # 'svc__gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-        # 'kbest__k': [1, 2],
-        # 'mlp__activation': ['identity', 'logistic', 'tanh', 'relu'],
-        # 'mlp__solver': ['lbfgs', 'sgd', 'adam'],
-        # 'mlp__learning_rate': ['invscaling', 'adaptive'],
-        #  'mlpclassifier__activation': ['logistic', 'tanh', 'relu'],
-        #  'mlpclassifier__solver': ['lbfgs', 'sgd', 'adam'],
-        #  'mlpclassifier__learning_rate': ['invscaling', 'adaptive'],
-        # 'gb__n_estimators': [10, 20, 50, 70, 100, 120, 150],
-        # 'gb__max_depth': [3, 4, 5, 6, 7, 8, 9, 10, 15],
-        # 'gb__learning_rate': [0.1, 0.3, 0.6, 0.9, 1.2, 1.5],
-        # 'dt__max_depth': [3, 4, 5, 6, 7]
-        # 'kpercentile__percentile': [90],
-        # 'kpercentile__percentile': [90],
-        # 'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3), (1, 4)],
-        # 'tfidf__max_df': [0.4, 0.5, 0.6, 0.7, 0.8],
+
+def gridSearch_models(data):
+    numerical_features = ['Number of Donations', 'Months since First Donation', 'Months since Last Donation',
+                          'If']
+
+    y = data['Made Donation in March 2007']
+    X = data.loc[:, numerical_features]
+    print('X', X.head())
+    # X_x = PolynomialFeatures(2).fit_transform(X)
+    from sklearn import preprocessing
+    scaler = preprocessing.StandardScaler()
+    # X_x1 = PolynomialFeatures(2).fit_transform(X_x)
+    # X_train, X_test, y_train, y_test = train_test_split(X_x, y, test_size=0.2)
+    # print('X_train', X_train.shape)
+
+    sss = StratifiedShuffleSplit(n_splits=570, test_size=0.1)
+
+    # logit = LogisticRegression()
+    logit = LogisticRegressionCV(
+        Cs=list(np.power(3.0, np.arange(-10, 10)))
+        , penalty='l2'
+        , scoring='neg_log_loss'
+        , cv=sss
+        , random_state=789
+        , max_iter=10000
+        , fit_intercept=True
+        , solver='liblinear'
+        , tol=1e-4
+    )
+
+
+
+    bagging = BaggingClassifier(logit)
+    results = model_selection.cross_val_score(bagging, X, y, scoring='neg_log_loss', cv=sss)
+    print(results.mean())
+
+    '''
+    models1 = {
+        'logit': LogisticRegression(warm_start=True),
+        'svc': SVC(probability=True),
+        'mlp':  MLPClassifier(),
+        'forest': RandomForestClassifier(),
+        'dt': DecisionTreeClassifier(max_features="auto")
     }
+    '''
 
-    robustScale = StandardScaler(with_mean=False)
-    pca = decomposition.PCA()
-    logistic = LogisticRegression()
-    xgboost = xgb.XGBClassifier()
-    forest = RandomForestClassifier()
-    svc = SVC(probability=True)
-    DTC = DecisionTreeClassifier(max_features="auto", class_weight="balanced", max_depth=2)
-    nb = GaussianNB()
-    et = ExtraTreesClassifier()
-    ABC1 = AdaBoostClassifier(base_estimator=DTC)
-    ABC2 = AdaBoostClassifier(base_estimator=xgboost)
-    ABC3 = AdaBoostClassifier(base_estimator=SVC)
-    ABC4 = AdaBoostClassifier(base_estimator=forest)
-    ABC5 = AdaBoostClassifier(base_estimator=nb)
-    ABC6 = AdaBoostClassifier(base_estimator=et)
-    mlp = MLPClassifier(max_iter=100000)
-    gen = preprocessing.PolynomialFeatures(2)
-    base_estimator = BaseEstimator_compability()
-    gb = GradientBoostingClassifier(init=base_estimator)
-    # voting = VotingClassifier(estimators=[('ada1', ABC1), ('ada2', ABC2), ('ada3', ABC3), ('ada4', ABC4), ('ada5', ABC5), ('ada6', ABC6)],
-    #                          voting='hard')
-    #eclf = EnsembleVoteClassifier(clfs=[ABC1, ABC2], voting='soft')
-    pipe = Pipeline(steps=[('gen', gen), ('scale', robustScale), ('abC2', ABC2)])
-    import pprint as pp
-    #pp.pprint(sorted(eclf.get_params().keys()))
-    pp.pprint(sorted(pipe.get_params().keys()))
+    test_file = 'C:\\Users\\Jenny\\Documents\\Mathfreak_Data\\DataKind\\BloodDonation\\test.csv'
 
-    sss = StratifiedShuffleSplit(n_splits=3, test_size=0.1)
-    # sm = SMOTE()
-    # features_res, labels_res = sm.fit_sample(features, labels)
-    grid_search = GridSearchCV(pipe, param_grid=param_grid, scoring='neg_log_loss', cv=sss)
-    grid_search.fit(features, labels)
-    clf = grid_search.best_estimator_
-    print(grid_search.best_score_)
-
-    return clf
+    test = pd.read_csv(test_file)
+    test1 = make_feature(test)
+    test_X = test1.loc[:, numerical_features]
+    test_X_x = scaler.fit_transform(test_X)
+    # test_X_x1 = PolynomialFeatures(2).fit_transform(test_X)
 
 
-clf = model()
+    predict_proba = bagging.fit(X, y).predict_proba(test_X_x)
+    print(predict_proba)
+    test_col = pd.DataFrame(predict_proba)
+    df_id = test.loc[:, ['ID']]
+    test_mid = pd.concat([df_id, test_col], axis=1)
+    test_mid.head()
+    submission = test_mid.loc[:, ['ID', 1]]
+    submission.rename(columns={'ID': '', 1: 'Made Donation in March 2007'}, inplace=True)
+    submission.to_csv('submission.csv')
 
 
-from sklearn import metrics
-from sklearn.model_selection import cross_val_score
+if __name__ == '__main__':
+    make_feature(data)
+    gridSearch_models(data)
 
-scores = cross_val_score(clf, features, labels, cv=sss, scoring='neg_log_loss')
-scores.mean()
-
-'''
-from tester import test_classifier
-print(' ')
-# use test_classifier to evaluate the model
-# selected by GridSearchCV
-print("Tester Classification report")
-test_classifier(clf, data_raw, numerical_features)
-
-'''
